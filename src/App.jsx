8a94488ef24e85b94925a460e735b405f961081a
Rajhu Acosta <rajhu2005eloy@gmail.com>
2024-01-03 23:03:58 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
-import { useForm, ValidationError } from '@formspree/react';
-import Header from './components/Header';
-import Presentation from './components/Presentation';
-import PhilosophyAndValues from './components/PhilosophyAndValues';
-import MyProjects from './components/MyProjects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import SkillSet from './components/SkillSet';
-import './App.css';
+import { useEffect, useState } from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import Header from "./components/Header";
+import Presentation from "./components/Presentation";
+import PhilosophyAndValues from "./components/PhilosophyAndValues";
+import MyProjects from "./components/MyProjects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import SkillSet from "./components/SkillSet";
+import "./App.css";
 
 function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -15,24 +15,24 @@ function App() {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
   return (
-    <div className="font-[Sen] min-h-screen">
-      <Header windowWidth={windowWidth}/>
-      <Presentation/>
+    <div id="home" className="font-[Sen] min-h-screen">
+      <Header windowWidth={windowWidth} />
+      <Presentation />
       {/* <WorkExperience/> */}
-      <PhilosophyAndValues/>
-      <SkillSet/>
-      <MyProjects/>
+      <PhilosophyAndValues />
+      <SkillSet />
+      <MyProjects />
       {/* <Dribbble/> */}
-      <Contact useForm={useForm} ValidationError={ValidationError}/>
-      <Footer windowWidth={windowWidth}/>
+      <Contact useForm={useForm} ValidationError={ValidationError} />
+      <Footer windowWidth={windowWidth} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/PhilosophyAndValues.jsx
+++ src/components/PhilosophyAndValues.jsx
@@ -1,24 +1,36 @@
 const PhilosophyAndValues = () => {
   return (
     <div id="aboutMe" className="sm:grid sm:grid-cols-[1fr_1fr]">
-        <div className="bg-[#FFF3E4] px-4 xsm:px-7 xl:px-12 3xl:px-20 py-10 lg:py-16 grid gap-5 sm:block">
-            <p className="text-3xl 3xl:text-5xl font-bold text-[#181823] sm:mb-6 3xl:mb-10">
-                Philosophy & Values
-            </p>
-            <p className="text-[#5B6876] text-base 3xl:text-2xl">
-                I think everyone wants the same thing - relationship with humanity, peace with the metaphysical, and experience with the universe. I try to grasp these things with my values: authenticity, creativity, & hospitality.
-            </p>
-            <a className="block mt-8 lg:mt-16 3xl:" href="https://www.linkedin.com">
-                <span className="pb-1 border-b-2 border-[#89949f] hover:border-0 hover:text-[#89949f] font-medium 3xl:text-2xl">
-                    <a href="https://www.linkedin.com/in/rajhu-acosta--066976253" target="_blank">Check my Linkedin</a>
-                </span>
+      <div className="bg-[#FFF3E4] px-4 xsm:px-7 xl:px-12 3xl:px-20 py-10 lg:py-16 grid gap-5 sm:block">
+        <p className="text-3xl 3xl:text-5xl font-bold text-[#181823] sm:mb-6 3xl:mb-10">
+          Filosofía y valores
+        </p>
+        <p className="text-[#5B6876] text-base 3xl:text-2xl">
+          En el fondo, creo que todos anhelamos las mismas cosas: una conexión
+          con la humanidad, una paz con lo metafísico y una experiencia
+          auténtica del universo. Estos anhelos los busco y vivo a través de mis
+          valores: la autenticidad, la creatividad y la hospitalidad.
+        </p>
+        <a className="block mt-8 lg:mt-16 3xl:" href="https://www.linkedin.com">
+          <span className="pb-1 border-b-2 border-[#89949f] hover:border-0 hover:text-[#89949f] font-medium 3xl:text-2xl">
+            <a
+              href="https://www.linkedin.com/in/rajhu-acosta--066976253"
+              target="_blank"
+            >
+              Revisa mi Linkedin
             </a>
-        </div>
-        <picture>
-            <img className="sm:h-full" src="/pr.jpeg" alt="programadores trabajando juntos" />
-        </picture>
+          </span>
+        </a>
+      </div>
+      <picture>
+        <img
+          className="sm:h-full"
+          src="/pr.jpeg"
+          alt="programadores trabajando juntos"
+        />
+      </picture>
     </div>
-  )
-}
+  );
+};
 
-export default PhilosophyAndValues
+export default PhilosophyAndValues;
